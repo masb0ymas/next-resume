23d3d1244081086a78b6ba9db633d12a6adb395f
masb0ymas <fajriajjha@gmail.com>
2020-09-09 22:58:07 +0700

    fix: landing education

--- src/views/Landing/partials/Education.tsx
+++ src/views/Landing/partials/Education.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Card from 'components/Card/Card'
-import SectionEducation from 'components/Section/partials/Education'
+import Section from 'components/Section/partials/Education'
 
 function Education() {
   return (
-    <SectionEducation
-      title={'Education & Experience'}
+    <Section
+      title={'Education & Certification'}
       subtitle={`
-      My education and experience started from college until now
+      Education and certification that I have obtained
       `}
     >
       <React.Fragment>
@@ -19,28 +19,8 @@ function Education() {
               sed do eiusmod tempor incidiei dunt ut labore et dolore magna aliqua. 
               Ut enim ad minim veniam quis nostrud.`}
         />
-
-        <Card
-          title={'Minang IT Camp'}
-          subtitle={'Komunitas Programming'}
-          periode={'Oktober 2017 - Present'}
-          text={`Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-              sed do eiusmod tempor incidiei dunt ut labore et dolore magna aliqua. 
-              Ut enim ad minim veniam quis nostrud.`}
-        />
-
-        <div />
-
-        <Card
-          title={'Nusantech'}
-          subtitle={'Software Enginner ( Full time )'}
-          periode={'Juni 2019 - Present'}
-          text={`Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-              sed do eiusmod tempor incidiei dunt ut labore et dolore magna aliqua. 
-              Ut enim ad minim veniam quis nostrud.`}
-        />
       </React.Fragment>
-    </SectionEducation>
+    </Section>
   )
 }
 
