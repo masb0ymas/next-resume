91c9c8944a69fed476cd255877dd5390d86dad53
masb0ymas <fajriajjha@gmail.com>
2020-09-09 22:57:46 +0700

    fix: landing experience

--- src/views/Landing/Landing.tsx
+++ src/views/Landing/Landing.tsx
@@ -3,6 +3,7 @@ import cx from 'classnames'
 import About from 'views/Landing/partials/About'
 import Skill from 'views/Landing/partials/Skill'
 import Education from 'views/Landing/partials/Education'
+import Experience from 'views/Landing/partials/Experience'
 
 function Landing() {
   return (
@@ -15,6 +16,7 @@ function Landing() {
       <About />
       <Skill />
       <Education />
+      <Experience />
     </React.Fragment>
   )
 }
