da0272039bbeb9052c1fec929171e1641d9e006b
masb0ymas <fajriajjha@gmail.com>
2020-09-09 22:04:27 +0700

    fix: landing

--- src/views/Landing/Landing.tsx
+++ src/views/Landing/Landing.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import cx from 'classnames'
 import About from 'views/Landing/partials/About'
 import Skill from 'views/Landing/partials/Skill'
 import Education from 'views/Landing/partials/Education'
@@ -6,6 +7,11 @@ import Education from 'views/Landing/partials/Education'
 function Landing() {
   return (
     <React.Fragment>
+      <section className={cx('text-lg')}>
+        <div className={cx('section')}>
+          <div className={cx('container mx-auto')} />
+        </div>
+      </section>
       <About />
       <Skill />
       <Education />
