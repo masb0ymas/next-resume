f5ab5d4343b23b8f4460a4e60ecc8193bd1c739c
masb0ymas <fajriajjha@gmail.com>
2021-01-19 22:08:39 +0700

    fix: shadow card image

--- src/components/Card/CardImage.tsx
+++ src/components/Card/CardImage.tsx
@@ -13,55 +13,50 @@ function CardImage(props: CardImageProps) {
   const { imageCover, imageLogo, title, subtitle, periode } = props
   return (
     <React.Fragment>
-      <div className={cx('my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3')}>
-        <article
-          className={cx('bg-white overflow-hidden rounded-lg shadow-lg')}
+      <article className={cx('bg-white overflow-hidden rounded-lg shadow hover:shadow-xl transition duration-500 ease-in-out')}>
+        <a href="/">
+          <img
+            alt="Placeholder"
+            className={cx('block h-auto w-full')}
+            src={imageCover}
+          />
+        </a>
+
+        <header
+          className={cx(
+            'flex items-center justify-between leading-tight p-2 md:p-4',
+          )}
         >
-          <a href="/">
-            <img
-              alt="Placeholder"
-              className={cx('block h-auto w-full')}
-              src={imageCover}
-            />
-          </a>
-
-          <header
-            className={cx(
-              'flex items-center justify-between leading-tight p-2 md:p-4',
-            )}
-          >
-            <h1 className={cx('text-lg')}>
-              <a
-                className={cx('no-underline hover:underline text-black')}
-                href="/"
-              >
-                {title}
-              </a>
-            </h1>
-            <p className={cx('text-grey-darker text-sm')}>{periode}</p>
-          </header>
-
-          <footer
-            className={cx(
-              'flex items-center justify-between leading-none p-2 md:p-4',
-            )}
-          >
+          <h1 className={cx('text-lg')}>
             <a
-              className={cx(
-                'flex items-center no-underline hover:underline text-black',
-              )}
+              className={cx('no-underline hover:underline text-black')}
               href="/"
             >
-              <img
-                alt="Placeholder"
-                className={cx('block h-auto w-10 rounded-full')}
-                src={imageLogo}
-              />
-              <p className={cx('ml-4 text-md')}>{subtitle}</p>
+              {title}
             </a>
-          </footer>
-        </article>
-      </div>
+          </h1>
+        </header>
+        <p className={cx('text-gray-600 text-sm px-2 md:px-4')}>{periode}</p>
+        <footer
+          className={cx(
+            'flex items-center justify-between leading-none p-2 md:p-4',
+          )}
+        >
+          <a
+            className={cx(
+              'flex items-center no-underline hover:underline text-black',
+            )}
+            href="/"
+          >
+            <img
+              alt="Placeholder"
+              className={cx('block h-auto w-10 rounded-full')}
+              src={imageLogo}
+            />
+            <p className={cx('ml-4 text-md')}>{subtitle}</p>
+          </a>
+        </footer>
+      </article>
     </React.Fragment>
   )
 }
