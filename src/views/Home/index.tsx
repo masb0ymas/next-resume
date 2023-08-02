0aed2a0e94949398a459a177be5443cc6545a2d0
masb0ymas <n.fajri@outlook.com>
2023-08-02 19:26:06 +0700

    feat: add scroll to top

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,3 +1,6 @@
+import { ActionIcon, Affix, Tooltip, Transition, rem } from '@mantine/core'
+import { useWindowScroll } from '@mantine/hooks'
+import { IconArrowUp } from '@tabler/icons-react'
 import EducationSection from './partials/Education'
 import ExperienceSection from './partials/Experience'
 import GithubSection from './partials/Github'
@@ -5,6 +8,8 @@ import HeroSection from './partials/Hero'
 import PortfolioSection from './partials/Portfolio'
 
 function LandingPage() {
+  const [scroll, scrollTo] = useWindowScroll()
+
   return (
     <div>
       <HeroSection />
@@ -16,6 +21,27 @@ function LandingPage() {
       <ExperienceSection />
 
       <GithubSection />
+
+      <Affix position={{ bottom: rem(65), left: rem(30) }}>
+        <Transition transition="slide-up" mounted={scroll.y > 0}>
+          {(transitionStyles) => (
+            <Tooltip
+              label="Scroll to top"
+              transitionProps={{ transition: 'pop', duration: 300 }}
+            >
+              <ActionIcon
+                color="blue"
+                variant="filled"
+                size="lg"
+                style={transitionStyles}
+                onClick={() => scrollTo({ y: 0 })}
+              >
+                <IconArrowUp size="1rem" />
+              </ActionIcon>
+            </Tooltip>
+          )}
+        </Transition>
+      </Affix>
     </div>
   )
 }
