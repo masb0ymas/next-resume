27a3896cb6f88836d5ae0fdccddf6646ca97ee63
masb0ymas <n.fajri@outlook.com>
2024-10-20 13:15:55 +0700

    fix: update carousel

--- src/components/carousel/LogoCarousel.tsx
+++ src/components/carousel/LogoCarousel.tsx
@@ -1,19 +1,20 @@
-type IProps = {
-  data: {
-    src: string
-    alt: string
-  }[]
-}
+const logos = [
+  { src: "/static/brand/slack.svg", alt: "Slack" },
+  { src: "/static/brand/sentry.svg", alt: "Sentry" },
+  { src: "/static/brand/framer.svg", alt: "Framer" },
+  { src: "/static/brand/cloudflare.svg", alt: "Cloudflare" },
+  { src: "/static/brand/docker.svg", alt: "Docker" },
+  { src: "/static/brand/github.svg", alt: "Github" },
+  { src: "/static/brand/atlassian.svg", alt: "Atlassian" },
+]
 
-export default function LogoCarousel(props: IProps) {
-  const logos = props.data
-
+export default function LogoCarousel() {
   return (
     <div className="inline-flex w-full flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
       <ul className="flex animate-infinite-scroll items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8">
-        {logos.map((logo, index) => (
+        {logos.map((item, index) => (
           <li key={index}>
-            <img src={logo.src} alt={logo.alt} />
+            <img src={item.src} alt={item.alt} />
           </li>
         ))}
       </ul>
@@ -21,9 +22,9 @@ export default function LogoCarousel(props: IProps) {
         className="flex animate-infinite-scroll items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8"
         aria-hidden="true"
       >
-        {logos.map((logo, index) => (
+        {logos.map((item, index) => (
           <li key={index}>
-            <img src={logo.src} alt={logo.alt} />
+            <img src={item.src} alt={item.alt} />
           </li>
         ))}
       </ul>
--- src/components/carousel/ProjectCarousel.tsx
+++ src/components/carousel/ProjectCarousel.tsx
@@ -1,37 +1,47 @@
+import clsx from "clsx"
+
 type IProps = {
   data: {
-    src: string
-    alt: string
+    thumbnail: string
+    title: string
+    slug: string | null
   }[]
+  imgStyle?: string
 }
 
 export default function ProjectCarousel(props: IProps) {
-  const logos = props.data
+  const projects = props.data
+  const { imgStyle } = props
+
+  function Carousel() {
+    return projects.map((item, index) => {
+      const link = item.slug ? `/project/${item.slug}` : "#"
+
+      return (
+        <li key={index} className="text-center">
+          <a href={link}>
+            <img
+              className={clsx("h-72 w-auto mb-2", imgStyle)}
+              src={item.thumbnail}
+              alt={item.title}
+            />
+            <span className="text-gray-200 font-semibold">{item.title}</span>
+          </a>
+        </li>
+      )
+    })
+  }
 
   return (
     <div className="inline-flex w-full flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
       <ul className="flex animate-infinite-scroll items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-4">
-        {logos.map((item, index) => (
-          <li key={index} className="text-center">
-            <a href="#">
-              <img className="h-72 w-auto mb-2" src={item.src} alt={item.alt} />
-              <span className="text-gray-200">{item.alt}</span>
-            </a>
-          </li>
-        ))}
+        {Carousel()}
       </ul>
       <ul
         className="flex animate-infinite-scroll items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-4"
         aria-hidden="true"
       >
-        {logos.map((item, index) => (
-          <li key={index} className="text-center">
-            <a href="#">
-              <img className="h-72 w-auto mb-2" src={item.src} alt={item.alt} />
-              <span className="text-gray-200">{item.alt}</span>
-            </a>
-          </li>
-        ))}
+        {Carousel()}
       </ul>
     </div>
   )
